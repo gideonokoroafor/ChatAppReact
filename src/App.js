0b7952e17af5b756ca599f12e9b85ed3d3d6c926
Gideon Okoroafor <okoro062@umn.edu>
2023-09-20 17:17:43 -0500

    implemented chat engine

--- src/App.js
+++ src/App.js
@@ -1,2 +1,15 @@
 import { ChatEngine } from "react-chat-engine";
 import './App.css'
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="1000vh"
+            projectID=""
+            userName=""
+            userSecret=""
+        />
+    )
+}
+
+export default App;
